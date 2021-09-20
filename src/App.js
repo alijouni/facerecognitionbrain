0ad67d010b5ba42861bd63adaef13dcb30c51511
Ali Jouni <alijouni@outlook.de>
2021-09-20 23:54:25 +0300

    Working on Image Recognition API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React,{Component} from 'react'
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: "1cf7d49421e1474caa34a9293f3d0592",
+});
 
 const particlesOptions= {
   particles: {
@@ -17,19 +24,53 @@ const particlesOptions= {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-              params={particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank/>
-      <ImageLinkForm />
-      {/* <FaceRecognition/> */}
-    </div>
-  ); 
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageURL:''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input:event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageURL:this.state.input})
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(
+      function (response) {
+        
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function (err) {
+        console.log(err);
+      }
+    )
+  }
+  
+  render() {
+
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageURL={this.state.imageURL}/>
+      </div>
+    );
+  }
 }
 
 export default App;
