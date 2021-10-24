9c311c10dd91942c1ee79bd448664f7757ff3687
Ali Jouni <alijouni@outlook.de>
2021-10-24 09:30:40 +0300

    Upload fix again

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,36 +8,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit }) => {
     const onButtonClick = () => {
         // `current` points to the mounted file input element
        inputFile.current.click();
-       this.setState({ imageURL: this.state.input });
-    fetch('https://safe-scrubland-81316.herokuapp.com/imageurl', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        input: this.state.input
-
-      })
-    })
-    .then(response=> response.json())
-    .then(response => {
-      if (response) {
-        fetch('https://safe-scrubland-81316.herokuapp.com/image', {
-          method: 'put',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            id: this.state.user.id
-  
-          })
-        })
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, { entries: count }))
-        })
-        .catch(console.log);
-      }
-      this.displayFaceBox(this.calculateFaceLocation(response));
-    })
-    .catch(err => console.log(err));
-
+       onButtonSubmit();
       };
     return (
         <div>
