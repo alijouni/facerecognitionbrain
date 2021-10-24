444904251503b5375f83683cbb464a501ac54c58
Ali Jouni <alijouni@outlook.de>
2021-10-24 09:23:49 +0300

    Upload fix

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,6 +8,36 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit }) => {
     const onButtonClick = () => {
         // `current` points to the mounted file input element
        inputFile.current.click();
+       this.setState({ imageURL: this.state.input });
+    fetch('https://safe-scrubland-81316.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+
+      })
+    })
+    .then(response=> response.json())
+    .then(response => {
+      if (response) {
+        fetch('https://safe-scrubland-81316.herokuapp.com/image', {
+          method: 'put',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            id: this.state.user.id
+  
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }))
+        })
+        .catch(console.log);
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response));
+    })
+    .catch(err => console.log(err));
+
       };
     return (
         <div>
@@ -21,7 +51,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit }) => {
                         className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
                         onClick={onButtonSubmit}
                     >Detect</button>
-                    <input className='f4 pa2 w-70 center' type='file' id='file' ref={inputFile} style={{display: 'none'}}/>
+                    <input className='f4 pa2 w-70 center' type='file' id='file' ref={inputFile} style={{display: 'none'}} onChage={onInputChange}/>
                     <button
                         className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
                         onClick={onButtonClick}
