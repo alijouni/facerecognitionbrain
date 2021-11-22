5a0e3ee4573be9f75da3b0e5192951e59df6553b
Ali Jouni <alijouni@outlook.de>
2021-11-22 16:33:19 +0200

    Invalid Credentials

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -6,7 +6,8 @@ class Signin extends React.Component {
         super();
         this.state = {
             signInEmail: '',
-            signInPassword:''
+            signInPassword:'',
+            isValid: true
         }
     }
     onEmailChange = (event) => {
@@ -31,6 +32,10 @@ class Signin extends React.Component {
                 if (user.id) {
                     this.props.loadUser(user);
                     this.props.onRouteChange('home');
+                    this.setState({isValid:true});
+          }
+          else{
+              this.setState({isValid:false});
           }
         })
     }
@@ -43,6 +48,11 @@ class Signin extends React.Component {
                     <div className="measure ">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                            {this.state.isValid=== false ? 
+                            <div>
+                               Invalid Credentials 
+                            </div>
+                            :<div></div>}
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                                 <input
